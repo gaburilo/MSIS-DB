cc6e20c41b2bccf65de52fd962166dcdc7fc5a13
gabirrivera89 <gabirrivera@gmail.com>
2021-10-31 10:42:57 -0600

    Update UpdateProductPriceHist.sql

--- UpdateProductPriceHist.sql
+++ UpdateProductPriceHist.sql
@@ -1,3 +1,25 @@
+/* A View */
+
+CREATE OR ALTER VIEW customerTrainer AS
+	SELECT c.CustomerID,
+			CONCAT(c.Firstname,' ',c.Lastname) as CustomerName,
+			ce.Email,cr.CourseName,st.StudioName,cl.Date,Time
+			,CONCAT(em.Firstname,' ',em.Lastname) as TrainerName
+	From Customer AS c
+	LEFT OUTER JOIN Customer_Email AS ce ON c.CustomerID=ce.CustomerID
+	INNER JOIN Membership AS m ON c.CustomerID=m.CustomerID  
+	INNER JOIN Schedule AS s ON m.MembershipID=s.MembershipID  
+	INNER JOIN Class AS cl ON s.ClassID=cl.ClassID
+	INNER JOIN Course as cr ON cl.CourseID=cr.CourseID
+   	INNER JOIN Trainer as tr ON cl.EmployeeID=tr.EmployeeID
+	INNER JOIN Employee as em ON tr.EmployeeID=em.EmployeeID
+	INNER JOIN Studio AS st ON cl.StudioID=st.StudioID;
+
+Select * from CustomerTrainer
+Order by CustomerName;
+
+
+-- Trigger
 CREATE TRIGGER update_ProductPrice_History ON dbo.Product
 AFTER UPDATE
 AS
